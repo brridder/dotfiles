e5534ecf3d85fb44b7a4c031bc690f63fe1d8c3a
Ben Ridder <ben@mindtribe.com>
2019-01-07 15:08:14 -0800

    update gdbinit

--- install.sh
+++ install.sh
@@ -7,17 +7,17 @@ git submodule update --init --recursive
 # Link each file to the home directory iff the file does not exist or is
 # already a symbolic link. This will probably annoy someone, eventually.
 for target in files/*; do
-	link_name=$HOME/.`basename $target`
-	if [ -e $link_name -a ! -h $link_name ]; then
-		echo "Skipping $link_name; not a symbolic link"
-	else
-		echo "Installing $link_name..."
-		ln -fs -T $PWD/$target $link_name
-	fi
+        link_name=$HOME/.`basename $target`
+        if [ -e $link_name -a ! -h $link_name ]; then
+                echo "Skipping $link_name; not a symbolic link"
+        else
+                echo "Installing $link_name..."
+                ln -fs -T $PWD/$target $link_name
+        fi
 done
 
 echo "Updating Command-T..."
-extdir=files/vim/bundle/command-t/ruby/command-t/ext/command-t
+extdir=files/vim/bundle/command-t/ruby/command-t/
 (cd $extdir && ruby extconf.rb && make all)
 
 echo "Updating lesskey file..."
